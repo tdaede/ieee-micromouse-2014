52fc3389d8521ef3d12df519556057133c2454ae
Thomas Daede <daede003@umn.edu>
2014-04-24 15:57:39 -0500

    STUFF

--- micromouse2014.ino
+++ micromouse2014.ino
@@ -6,6 +6,8 @@ DFRobot2WD robot = DFRobot2WD();
 #define IR_THRESHOLD 2.0f
 #define IR_DEBUG 0
 
+#define GRIDCROSS_PAUSE 1000
+
 
 #define STATE_LINEFOLLOW 0
 #define STATE_GRIDCROSS 1
@@ -22,12 +24,63 @@ int motor_velocity[2];
 #define MOTOR_SLIGHT_TURN 96
 #define MOTOR_HARD_TURN 64
 
+int x = 0;
+int y = 0;
+int d = 0;
+
+int newx;
+int newy;
+
+int decideTurn;
+
+char grid[32][32];
+
+#define C_FWD 1
+#define C_TRN 2
+#define C_END 0
+
+int cheatIndex = 0;
+
+char cheat[] = {
+  C_TRN,
+  C_FWD,
+  C_FWD,
+  C_TRN,
+  C_TRN,
+  C_TRN,
+  C_FWD,
+  C_TRN,
+  C_FWD,
+  C_FWD,
+  C_TRN,
+  C_FWD,
+  C_TRN,
+  C_FWD,
+  C_TRN,
+  C_TRN,
+  C_TRN,
+  C_FWD,
+  C_TRN,
+  C_TRN,
+  C_TRN,
+  C_FWD,
+  C_FWD,
+  C_END
+};
 
 void setup()
 {
   motor_velocity[0] = 0;
   motor_velocity[1] = 0;
   Serial.begin(9600);
+  randomSeed(69);
+  for (x = 0; x < 32; x++) {
+    for (y = 0; y < 32; y++) {
+      grid[x][y] = 0;
+    }
+  }
+  x = 16;
+  y = 16;
 }
 
 #define MOTOR_LEFT 0
@@ -156,7 +209,7 @@ void loop() {
     cycleCount++;
     digitalWrite(LED_RED,HIGH);
     digitalWrite(LED_GREEN,LOW);
-    if (cycleCount > 1000) {
+    if (cycleCount > GRIDCROSS_PAUSE) {
       state = STATE_SCAN;
       cycleCount = 0;
     }
@@ -165,10 +218,68 @@ void loop() {
     motorSet(MOTOR_LEFT,0);
     motorSet(MOTOR_RIGHT,0);
     sendPulse();
-    if (!digitalRead(IR_IN)) {
+    decideTurn = !digitalRead(IR_IN);
+    newx = x;
+    newy = y;
+    switch(d) {
+      case 0:
+        newy++;
+        break;
+      case 1:
+        newx++;
+        break;
+      case 2:
+        newy--;
+        break;
+      case 3:
+        newx--;
+        break;
+    }
+    if (!decideTurn) {
+      decideTurn = random(100) < (100 - 100/(grid[newx][newy]+1));
+    }
+    if (cheat[cheatIndex] != C_END) {
+      if (cheat[cheatIndex] == C_TRN) decideTurn = 1;
+      if (cheat[cheatIndex] == C_FWD) decideTurn = 0;
+    }
+    if (decideTurn) {
       state = STATE_ROTATING_OUT;
+      d++;
+      if (d == 4) d = 0;
     } else {
       state = STATE_LINEFOLLOW;
+      if (d == 0) {
+        y++;
+      } else if (d == 1) {
+        x++;
+      } else if (d == 2) {
+        y--;
+      } else if (d == 3) {
+        x--;
+      }
+      if (x < 0) x = 0;
+      if (y < 0) y = 0;
+      grid[x][y]++;
+      Serial.println("-----------------");
+      Serial.print("x: ");
+      Serial.print(x);
+      Serial.print("\ty: ");
+      Serial.print(y);
+      Serial.print("\tnewx: ");
+      Serial.print(newx);
+      Serial.print("\tnewy: ");
+      Serial.print(newy);
+      Serial.print("\n");
+      /*
+      for (newy = 0; newy < 32; newy++) {
+        for (newx = 0; newx < 32; newx++) {
+          Serial.print((int)grid[newx][newy]);
+          Serial.print(" ");
+        }
+        Serial.print("\n");
+      }
+      */
+      cheatIndex++;
     }
   }
   if (state == STATE_ROTATING_OUT) {
